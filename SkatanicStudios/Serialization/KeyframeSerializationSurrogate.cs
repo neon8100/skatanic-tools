cb7093befef62a5c4a3b241de94a70ebff4206a0
Matt Gambell <matt@gsproductions.co.uk>
2018-06-07 12:06:21 +0100

    Initial commit. Serialisation system with surrages for animation curves, keyframes and vector 3

---
+++ SkatanicStudios/Serialization/KeyframeSerializationSurrogate.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.Serialization;
+
+namespace SkatanicStudios.Serialization.Surrogates
+{
+    public class KeyframeSerializationSurrogate : ISerializationSurrogate
+    {
+        public void GetObjectData(object obj, SerializationInfo info, StreamingContext context)
+        {
+            Keyframe keyframe = (Keyframe)obj;
+            info.AddValue("inTangent", keyframe.inTangent);
+            info.AddValue("outTangent", keyframe.outTangent);
+            info.AddValue("tangentMode", keyframe.tangentMode);
+            info.AddValue("time", keyframe.time);
+            info.AddValue("value", keyframe.value);
+        }
+
+        public object SetObjectData(object obj, SerializationInfo info, StreamingContext context, ISurrogateSelector selector)
+        {
+            Keyframe keyframe = (Keyframe)obj;
+            keyframe.inTangent = (float)info.GetValue("inTangent", typeof(float));
+            keyframe.outTangent = (float)info.GetValue("outTangent", typeof(float));
+            keyframe.tangentMode = (int)info.GetValue("tangentMode", typeof(int));
+            keyframe.time = (float)info.GetValue("time", typeof(float));
+            keyframe.value = (float)info.GetValue("value", typeof(float));
+
+
+            obj = keyframe;
+            return obj;
+        }
+    }
+}
+
---
+++ SkatanicStudios/Serialization/SerializationManager.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+
+namespace SkatanicStudios.Serialization
+{
+    public class SerializationManager
+    {
+
+        public static bool Save(string savename, object saveData)
+        {
+
+            BinaryFormatter binary = GetBinaryFormatter();
+
+            if (!Directory.Exists(Application.persistentDataPath + "/saves"))
+            {
+                Directory.CreateDirectory(Application.persistentDataPath + "/saves");
+            }
+
+            string path = Application.persistentDataPath + "/saves/" + savename + ".rpgtr";
+
+            FileStream file = File.Create(path);
+
+            binary.Serialize(file, saveData);
+
+            file.Close();
+
+            return true;
+
+        }
+
+        public static string[] GetSaves()
+        {
+            if (!Directory.Exists(Application.persistentDataPath + "/saves/"))
+            {
+                Directory.CreateDirectory(Application.persistentDataPath + "/saves/");
+            }
+
+            return Directory.GetFiles(Application.persistentDataPath + "/saves/");
+        }
+
+        public static SaveGame Load(string path)
+        {
+            if (!File.Exists(path))
+            {
+                return null;
+            }
+
+            BinaryFormatter binary = GetBinaryFormatter();
+
+            FileStream file = File.Open(path, FileMode.Open);
+            SaveGame save = (SaveGame)binary.Deserialize(file);
+            file.Close();
+
+            return save;
+        }
+
+        public static BinaryFormatter GetBinaryFormatter()
+        {
+            BinaryFormatter binary = new BinaryFormatter();
+            // 2. Construct a SurrogateSelector object
+            SurrogateSelector ss = new SurrogateSelector();
+
+
+            Vector3SerializationSurrogate v3ss = new Vector3SerializationSurrogate();
+            ss.AddSurrogate(typeof(Vector3),
+                            new StreamingContext(StreamingContextStates.All),
+                            v3ss);
+
+            KeyframeSerializationSurrogate kss = new KeyframeSerializationSurrogate();
+            ss.AddSurrogate(typeof(Keyframe), new StreamingContext(StreamingContextStates.All), kss);
+
+            AnimationCurveSerializationSurrogate acss = new AnimationCurveSerializationSurrogate();
+            ss.AddSurrogate(typeof(AnimationCurve), new StreamingContext(StreamingContextStates.All), acss);
+
+
+            // 5. Have the formatter use our surrogate selector
+            binary.SurrogateSelector = ss;
+
+            return binary;
+        }
+    }
+}
